206f49180449fa68e869fd3e7e5743456bdd7bd4
Gustavo Caldeira <caldeira.guga@gmail.com>
2026-07-14 11:25:23 -0300

    chore: setup inicial do projeto Spring Boot

---
+++ cortae/src/main/java/com/cortae/cortae/CortaeApplication.java
@@ -0,0 +1,13 @@
+package com.cortae.cortae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CortaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CortaeApplication.class, args);
+	}
+
+}
---
+++ cortae/src/test/java/com/cortae/cortae/CortaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cortae.cortae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CortaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
